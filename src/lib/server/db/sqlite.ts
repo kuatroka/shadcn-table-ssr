854184d77ef518d690e946977e9b8456417e94d3
kuatroka <aaarmani_2000@yahoo.com>
2024-02-28 23:40:00 +0000

    .

--- src/lib/server/db/sqlite.ts
+++ src/lib/server/db/sqlite.ts
@@ -15,18 +15,23 @@ export function getCik(
 		SELECT
 		COUNT(*) OVER () AS num_entries,
 		id,
-		cik,
-		cik_name,
+		l.cik,
+		r.cik_name,
 		cum_mean_twrr_per_cik_per_qtr_cons AS cum_twrr_cons,
+		cum_mean_twrr_per_cik_per_qtr_yahoo AS cum_twrr_yahoo,
 		(select count(*) from cik_md) AS initial_num_entries
-		FROM every_cik_latest_qtr_stats
+		FROM every_cik_latest_qtr_stats l
+		LEFT JOIN
+		(SELECT cik, cik_name
+		FROM cik_md) r
+		ON l.cik = r.cik
 		WHERE UPPER(cik_name) LIKE UPPER('%${q}%')
 		ORDER BY ${order_by ? order_by : 'cik'} ${order_dir ? order_dir : 'ASC'}
 		LIMIT $limit OFFSET $skip
 	`;
 	const stmnt = db.prepare(sql);
 	const rows = stmnt.all({ limit, q, skip });
-	console.log(rows.slice(0, 2));
-	console.log(rows.length);
+	// console.log(rows.slice(0, 2));
+	// console.log(rows.length);
 	return rows as Cik[];
 }
